97140253801a5afd97e8614e2f4d6cc0998c92ee
Leon Domiter <leon.domiter@hotmail.com>
2021-06-25 10:32:09 +0200

    stuff

--- src/main/java/com/bigbrain/cinema/config/WebSecurityConfig.java
+++ src/main/java/com/bigbrain/cinema/config/WebSecurityConfig.java
@@ -1,10 +1,23 @@
 package com.bigbrain.cinema.config;
 
+import com.bigbrain.cinema.security.JWTAuthenticationFilter;
+import com.bigbrain.cinema.security.JWTAuthorizationFilter;
+import com.bigbrain.cinema.security.SecurityConstants;
+import com.bigbrain.cinema.service.UserDetailsServiceImp;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+@Configuration
+@RequiredArgsConstructor
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final UserDetailsServiceImp userDetailsService;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -18,6 +31,11 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .headers().frameOptions().disable();
+    }
+
+    @Override
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder);
     }
 
 }
